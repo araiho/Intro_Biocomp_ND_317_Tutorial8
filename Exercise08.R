3726eae05a20cf3b9346f0a93ed136d43a576a6f
Logan <larnold3@nd.edu>
2017-10-22 20:23:57 -0400

    Finished Objective 1 for Problem 1

--- Exercise08.R
+++ Exercise08.R
@@ -5,18 +5,16 @@ library(stringr)
 readFile <- scan(file = "Cflorida.vcf", what = character(), sep = "\n")
 file.create("FixedData.vcf")
 
+#Creating regexpressions
 A <- "[Cc][Ff](07)?\\.[Aa]2?"
-B <- "[Cc][Gg](07)?\\.[Aa]2?"
-str_replace_all(readFile[2],B,"Cf.Gai")
+B <- "[Cc][Ff]\\.[Gg]([Aa][Ii])?2?"
 
 #Looping through contents of file
 for (i in 1:length(readFile)) {
   if(str_detect(readFile[i], "##")){
     write(readFile[i],file="FixedData.vcf",append=TRUE)
   } else if (str_detect(readFile[i], "#")) {
-    str_replace_all(readFile[i],A,"Cf.Sfa")
-    str_replace_all(readFile[i],B,"Cf.Gai")
-    write(readFile[i],file="FixedData.vcf",append=TRUE)
+    a <- str_replace_all(readFile[i],A,"Cf.Sfa")
+    write(str_replace_all(a,B,"Cf.Gai"),file="FixedData.vcf",append=TRUE)
   }
-  
 }
