3ff164cdcad3359fdafdc728f6771a047f9f6b11
Logan <larnold3@nd.edu>
2017-10-24 23:36:51 -0400

    Replacing missing data with NA

--- Exercise08.R
+++ Exercise08.R
@@ -8,8 +8,9 @@ file.create("FixedData.vcf")
 #Creating regexpressions
 A <- "[Cc][Ff](07)?\\.[Aa]2?"
 B <- "[Cc][Ff]\\.[Gg]([Aa][Ii])?2?"
-C <- "0/0:[0-9],[0-9]"
-D <- "[0-9],[0-9]"
+C <- "(0/0:[0-9],[0-9]|NA)"
+D <- "([0-9],[0-9]|NA)"
+E <- "\\./\\."
 
 #Looping through contents of file
 for (i in 1:length(readFile)) {
@@ -19,7 +20,8 @@ for (i in 1:length(readFile)) {
     a <- str_replace_all(readFile[i],A,"Cf.Sfa")
     write(str_replace_all(a,B,"Cf.Gai"),file="FixedData.vcf",append=TRUE)
   } else {
-  a <- str_extract_all(readFile[i], C)
+  a <- str_replace_all(readFile[i],E, "NA")
+  a <- str_extract_all(a, C)
   a <- str_extract_all(a, D)
   a <- paste( unlist(a), collapse=' ')
   write(a, file="FixedData.vcf", append=TRUE)
