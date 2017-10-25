7dc54127409dd5bf3517da0c84e0d24f26d53c61
Logan <larnold3@nd.edu>
2017-10-24 22:55:01 -0400

    Extracting allele counts

--- Exercise08.R
+++ Exercise08.R
@@ -8,6 +8,8 @@ file.create("FixedData.vcf")
 #Creating regexpressions
 A <- "[Cc][Ff](07)?\\.[Aa]2?"
 B <- "[Cc][Ff]\\.[Gg]([Aa][Ii])?2?"
+C <- "0/0:[0-9],[0-9]"
+D <- "[0-9],[0-9]"
 
 #Looping through contents of file
 for (i in 1:length(readFile)) {
@@ -16,5 +18,10 @@ for (i in 1:length(readFile)) {
   } else if (str_detect(readFile[i], "#")) {
     a <- str_replace_all(readFile[i],A,"Cf.Sfa")
     write(str_replace_all(a,B,"Cf.Gai"),file="FixedData.vcf",append=TRUE)
+  } else {
+  a <- str_extract_all(readFile[i], C)
+  a <- str_extract_all(a, D)
+  a <- paste( unlist(a), collapse=' ')
+  write(a, file="FixedData.vcf", append=TRUE)
   }
 }
