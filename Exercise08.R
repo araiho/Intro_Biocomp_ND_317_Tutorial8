f0e14b3bf209159bc7a0036e41c36670a26c3d83
shdavitt <shdavitt@gmail.com>
2017-10-26 17:35:46 -0400

    I agree with the changes to make the code more readable in a different way and have removed the residual code from my previous addition.

--- Exercise08.R
+++ Exercise08.R
@@ -1,37 +1,106 @@
 library(stringr)
 
+
+
 ######Problem 1
 
-readFile <- scan(file = "Cflorida.vcf", what = character(), sep = "\n")
-file.create("FixedData.vcf")
 
-#Creating regexpressions
-A <- "[Cc][Ff](07)?\\.[Aa]2?"
-B <- "[Cc][Ff]\\.[Gg]([Aa][Ii])?2?"
-C <- "([01]/[01]:[0-9],[0-9]|NA)"
+
+#Read in data and create new file for output
+
+readFile <- scan(file = "Cflorida.vcf", what = character(), sep = "\n")
+
+file.create("CfloridaCounts.vcf")
+
+
+
+###Creating regexpressions
+
+
+
+#To standardize samples
+
+A <- "[Cc][Ff](07)?\\.[Aa]2?" #For Texas
+
+B <- "[Cc][Ff]\\.[Gg]([Aa][Ii])?2?" #For FLorida
+
+
+
+#For allele counts, including possibility of NA
+
+C <- "([01]/[01]:[0-9],[0-9]|NA)" 
+
 D <- "([0-9],[0-9]|NA)"
+
+#For missing data
+
 E <- "\\./\\."
 
+
+
+#The beginning of each line
+
+header <- "Contig[0-9]{2,5}\\s[0-9]{1,2}\\sDeletedStuff\\sGT:AD:DP:GQ:PL"
+
+
+
 #Looping through contents of file
+
 for (i in 1:length(readFile)) {
+  
+  
+  
+  #Copy first line of original file
+  
   if(str_detect(readFile[i], "##")){
-    write(readFile[i],file="FixedData.vcf",append=TRUE)
+    
+    write(readFile[i],file="CfloridaCounts.vcf",append=TRUE)
+    
+    
+    
+    #Standardizing samples
+    
   } else if (str_detect(readFile[i], "#")) {
+    
     a <- str_replace_all(readFile[i],A,"Cf.Sfa")
-    write(str_replace_all(a,B,"Cf.Gai"),file="FixedData.vcf",append=TRUE)
+    
+    write(str_replace_all(a,B,"Cf.Gai"),file="CfloridaCounts.vcf",append=TRUE)
+    
+    
+    
+    #Extracting allele counts
+    
   } else {
-  a <- str_replace_all(readFile[i],E, "NA")
-  a <- str_extract_all(a, C)
-  a <- str_extract_all(a, D)
-  a <- paste( unlist(a), collapse='    ')
-  write(a, file="FixedData.vcf", append=TRUE)
+    
+    #Allele counts
+    
+    a <- str_replace_all(readFile[i],E, "NA") #Replace missing data with NA
+    
+    a <- str_extract_all(a, C) #Extract data containing allele count
+    
+    a <- str_extract_all(a, D) #Extract allele count only
+    
+    a <- paste( unlist(a), collapse='    ') #Convert into string
+    
+    
+    
+    #Header for each line
+    
+    b <- str_extract(readFile[i], header)
+    
+    
+    
+    #Combining allele counts and header
+    
+    c <- paste(b, a, sep=" ")
+    
+    
+    
+    #Writing to the file
+    
+    write(c, file="CfloridaCounts.vcf", append=TRUE)
+    
   }
+  
 }
 
-#Using shell and notepad, manually removed the "#" on the second line preceeding "CHROM" so that names of individuals are visible to read.table, and saved as "FixedDataNew.vcf".
-
-#Converted "FixedDataNew.vcf" to a table for easier reading.
-test=read.table(file="FixedDataNew.vcf", fill=TRUE)
-
-#Wrote the table to a new file, which is the same as originally file except the changes asked for in the assignment and the removal of the precceding "#" on the second line of the original data.
-write.table(test, file="FinalData.vcf")
