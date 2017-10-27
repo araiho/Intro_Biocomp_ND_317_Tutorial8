c1185520b90f646d227bb4ad27884d8a9208991d
Keith O'Connor <koconn23@nd.edu>
2017-10-26 21:40:10 -0400

    Final edits for Excercise 8 Problem 1 R code

--- ExerciseScript8_1.R
+++ ExerciseScript8_1.R
@@ -1,17 +1,17 @@
+## Keith O'Connor Excercise 8 Problem #1
+## This code will find and change the names of files from Florida and Texas to a set naming scheme (elseif statement in for loop).
+## Then it will simplify the allele data to allele counts (else part of for loop)
+## This is then exported to a new file called "CfloridaCounts.txt"
 library('stringr')
-
 ## Import Data
-
-
 Cflorida <- scan("~/Desktop/data-shell/Intro_Biocomp_ND_317_Tutorial8/Cflorida.vcf",what=character(),sep = "\n")
 #creates an empty file
 file.create("CfloridaCounts.txt")
 
-## Regular Expressions! Yay! The first two are standardize the nameing.
+## Regular Expressions! Yay! The first two are standardize the naming.
 ## The third regular expression is to find the allele counts for each individual.
 TX <- "[Cc][Ff](07)?\\.[Aa]2?"
 FL <- "[Cc][Ff]\\.(G2|GAI|gai)"
-## I'm not really sure about this one... I'm going to help tomorrow 3-5 to answer some questions
 SNP <- "[01.]/[01.]:([0-9.,]+):[0-9.]+:[0-9.]+:[0-9,.]+"
 
 for(i in 1:length(Cflorida)){
@@ -28,26 +28,3 @@ for(i in 1:length(Cflorida)){
     write(NSA,file="CfloridaCounts.txt",append=TRUE)
   }
 }
-
-  
-  
-  for (){ #look at old code to see how you looped over a file read in with scan
-    if (){ #how can you tell if this is the header line?
-      #write unchanged header line to file
-    }else if ()){ #how can you tell if this is the line with the column headings?
-      #standardize (replace) sample names with TX and FL regexes
-      #write new version of line to file
-    }else{ #now you're in the data
-      #replace full SNP info with allele counts only
-      #replace missing data with NA
-      #write new version of line to new file
-    }
-  }
-
-Cflorida <- read.delim("~/Desktop/data-shell/Intro_Biocomp_ND_317_Tutorial8/Cflorida.vcf")
-
-  str_replace_all(Cflorida[1,],TX,paste0("Cf.Sfa.",unlist(strsplit(as.character(Cflorida[1,]),"[.]"))))
-  
- 
-  
-
